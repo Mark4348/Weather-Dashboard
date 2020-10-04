68f9cc626552a99f860a58986af81df3439cefa6
MarkA <m_aguilar_2011@yahoo.com>
2020-10-04 00:05:09 -0500

    added layout skeleton with bootstrap and starting logic

---
+++ scripts.js
@@ -0,0 +1,25 @@
+//Variables
+
+
+const apiKEY ="c117cee7a459aec2dbaac55d5deab3ef";
+
+var queryURLBase = "api.openweathermap.org/data/2.5/forecast?q=" +  + "&appid=" + apiKEY;
+
+//Search Parameters
+
+
+
+//Functions
+
+
+
+
+
+
+
+//Main Processes
+
+//1.Retrieve user inputs and convert to variables
+//2. Use those variables to trun an AJAX calls to API, which will provie us with a JSON object
+//3. Retrieve each API object into useable fields
+//4.Generate html content onto page
